371270ba9f8b907f826fb8dba1c0a9b7e93515d7
xmyx588 <xmyx588@users.noreply.github.com>
2026-08-21 09:33:41 +0800

    Update PWA files

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'ai-image-editor-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(fetchResponse => {
+        if (fetchResponse && fetchResponse.status === 200) {
+          const responseClone = fetchResponse.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        }
+        return fetchResponse;
+      });
+    }).catch(() => caches.match('./index.html'))
+  );
+});
